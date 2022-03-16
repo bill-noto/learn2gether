13c0677dca7ef76629b40406991817345999d0c1
William Nonato <williamnonato9@gmail.com>
2022-03-16 18:17:34 +0100

    edited frontend of nova package, by creating original theme

---
+++ nova-components/Learn2gether/src/ThemeServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Admin\Learn2gether;
+
+use Illuminate\Support\ServiceProvider;
+use Laravel\Nova\Nova;
+
+class ThemeServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Nova::booted(function () {
+            Nova::theme(asset('/admin/learn2gether/theme.css'));
+        });
+
+        $this->publishes([
+            __DIR__.'/../resources/css' => public_path('admin/learn2gether'),
+        ], 'public');
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
